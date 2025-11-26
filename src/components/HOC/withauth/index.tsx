e062d351557196cb00c8137dde78a1eb8e7f9967
emre <mehmet.onen.job@gmail.com>
2025-11-26 23:20:51 +0300

    acıklama

--- src/components/HOC/withauth/index.tsx
+++ src/components/HOC/withauth/index.tsx
@@ -2,26 +2,27 @@ import { useEffect } from "react";
 import { history } from "../../../index";
 const WithAuth = (Component: Function) => {
 
-  
-    
+
+
     const Auth = (props: object) => {
 
-          useEffect(()=>{
-        console.log('sadasd')
-    },[])
+        useEffect(() => {
+            // Burada normalde getUserInfoile Token Check yapılabilir 401 alırsak logine pushlanırız 
+            // yapıyı görmek için utils/httpHelpera bakılabişlir
+        }, [])
 
-            const authenticatedUser = localStorage.getItem('access_token');
-            if (authenticatedUser==null) {
-                if(history.location.pathname!=="/register"){
-                    history.push('/login')
-                }
-            }else if(history.location.pathname=='/register' || history.location.pathname=="/login"){
-
-                history.push('/')
+        const authenticatedUser = localStorage.getItem('access_token');
+        if (authenticatedUser == null) {
+            if (history.location.pathname !== "/register") {
+                history.push('/login')
             }
-            return (
-                <Component {...props} />
-            )
+        } else if (history.location.pathname == '/register' || history.location.pathname == "/login") {
+
+            history.push('/')
+        }
+        return (
+            <Component {...props} />
+        )
     };
 
     return Auth;
